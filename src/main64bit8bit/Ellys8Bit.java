3dca9d1b41304180568d065e7de538ad3dd1bbca
unknown <jjbbss1993@gmail.com>
2017-09-13 22:55:25 -0500

    commit04

--- src/main64bit8bit/Ellys8Bit.java
+++ src/main64bit8bit/Ellys8Bit.java
@@ -1,53 +1,41 @@
 package main64bit8bit;
 
 public class Ellys8Bit {
- 
     public static void main(String[] args) {
         int decremento = 1;
         char[] num64bit8bit=inicializar();
-        
-        int N=80;
+        int N=5;
         char auxiliar;
-        boolean validar=true;
         boolean finalCero = false;
-        
-        
-        
         while(N-decremento>0){
             if (num64bit8bit[0]=='0')
                 finalCero = true;
             else
                 finalCero = false;
-            
             procesar(num64bit8bit);
-            
             if(finalCero){
                 num64bit8bit = moverCeros(num64bit8bit);
             }
-            
             decremento ++;
         }
-                  
         int decimal=calcular(num64bit8bit);
+        String binario="";
         for (int i= 0; i<64; i++){
-             System.out.println(num64bit8bit[i]);
+             binario=num64bit8bit[i]+binario;
         }
-       
+        System.out.println(binario);
         System.out.println(decimal);
     }
     
     public static char[] procesar(char[] num64bit8bit){
-        
         for (int i = 1; i < 64; i++) {               
             if (num64bit8bit[i]=='0' && num64bit8bit[i-1]=='1') {
-
                   char  auxiliar =num64bit8bit[i-1];
                     num64bit8bit[i-1]=num64bit8bit[i];
                     num64bit8bit[i]=auxiliar;
                     return num64bit8bit;
             }            
         } 
-        
         return num64bit8bit;
     }
     
@@ -75,28 +63,21 @@ public class Ellys8Bit {
     private static char[] moverCeros(char[] num64bit8bit) {
         boolean cambiar = true;
         int cantidadCeros = 0;
-        
-        //-----
         boolean hacercambio = false;
         boolean seguir = false;
-        
         int suma = 0;
         for (int i=0;i<64;i++){
             if (num64bit8bit[i]=='1'){
                 seguir = true;                
                 suma = suma + 1;
-                
-                if (suma == 8){seguir = false;
-                System.out.println("seguir falso>");}
-            }
-            
-            if (seguir && num64bit8bit[i]=='0'&& suma <8){
-                     hacercambio = true;
-                     System.out.println("hacer cambio");
+                if (suma == 8){
+                    seguir = false;
                 }
+            }
+            if (seguir && num64bit8bit[i]=='0'&& suma <8){
+                hacercambio = true;
+            }
         }
-        
-        //---
         if(hacercambio){            
             for(int i=0;i<64;i++){
                if (cambiar && num64bit8bit[i]=='1') {
@@ -118,5 +99,4 @@ public class Ellys8Bit {
         }
         return  num64bit8bit;       
     }
-    
 }
